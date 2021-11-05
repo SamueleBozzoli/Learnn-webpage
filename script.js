94a795a9bed52b1f810ce08d755244fb8d6d044f
SamueleBozzoli <93786402+SamueleBozzoli@users.noreply.github.com>
2021-11-05 18:43:34 +0100

    TOLTO NON FUNZIONE DI BACK TO TOP

--- script.js
+++ script.js
@@ -4,12 +4,3 @@ backBtn.addEventListener('click', function() {
     window.scrollTo(0, 0);
 });
 
-window.addEventListener('scroll', function() {
-    let posY = window.scrollY;
-
-    if(posY > 0) {
-        backBtn.style.display = 'block';
-    } else {
-        backBtn.style.display = 'none';
-    }
-}
